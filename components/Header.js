2647a22635075f4bbf9e94fa25ad4a69f973b55f
Dosybi <dosybi@gmail.com>
2022-11-17 18:51:39 +0600

    fix blog navigation padding

--- components/Header.js
+++ components/Header.js
@@ -27,28 +27,28 @@ const Header = ({ data }) => {
   return (
     <header className="mb-10">
       <div className="flex">
-        <div className="relative mr-4 w-11">
+        <div className="relative mr-4 w-11 md:w-14">
           <Link href="/">
             <Image data={data.avatar.responsiveImage} alt="" />
           </Link>
         </div>
-        <div className="text-xs">
-          <div className="font-bold">{data.title}</div>
-          <div>{data.description}</div>
-        </div>
-      </div>
-      <div className="ml-[3.75rem] flex">
-        <div className="flex">
-          <NavItem link={"/blog"} label={"Блог"} />
-          {data.menu.map((menuItem) => {
-            return (
-              <NavItem
-                link={`/blog/category/${menuItem.slug}`}
-                label={menuItem.category}
-                key={menuItem.id}
-              />
-            );
-          })}
+        <div className="flex flex-col justify-between">
+          <div className="text-xs">
+            <div className="font-bold">{data.title}</div>
+            <div>{data.description}</div>
+          </div>
+          <div className="flex">
+            <NavItem link={"/blog"} label={"Блог"} />
+            {data.menu.map((menuItem) => {
+              return (
+                <NavItem
+                  link={`/blog/category/${menuItem.slug}`}
+                  label={menuItem.category}
+                  key={menuItem.id}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </header>
